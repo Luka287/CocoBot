b13ccf61ab278a23cc778fd3f0f0c6cf73045fb6
Luka287 <Luka287@proton.me>
2023-03-04 15:53:28 +0400

    Initial

---
+++ src/test.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+const Add = (roma) =>{
+    if (roma === "Enter") {
+      
+      return (
+        <>
+        <h1 id='df'>scscscscsc</h1>
+        <style>  position: absolute;
+  background-color: red;</style>
+        </>
+      );
+      //console.log("yea");
+    }
+}
+
+
+export default Add;
